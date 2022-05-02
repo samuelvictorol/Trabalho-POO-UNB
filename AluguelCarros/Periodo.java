26de13279edc8771940c2f75318d178ca4a11ec7
Ana Hoffmann <87672491+AnHoff@users.noreply.github.com>
2022-05-02 09:53:44 -0300

    calcular diarias

--- AluguelCarros/Periodo.java
+++ AluguelCarros/Periodo.java
@@ -1,16 +1,12 @@
 package AluguelCarros;
 
-import java.time.Period;
-import java.util.Calendar;
-
 public class Periodo {
 	
-	private Calendar inicio = Calendar.getInstance();
-	private Calendar fim = Calendar.getInstance();
 	private String diaInicio, diaFim;
 	private String horaInicio, horaFim;
 	private String data;
-
+	private int diarias;
+	
 	public Periodo() {
 		
 	}
@@ -47,8 +43,7 @@ public class Periodo {
 		this.horaFim = horaFim;
 	}
 	
-	/*
-	public void formataInicioFim(String diaInicio, String diaFim, Calendar inicio, Calendar fim) {
+	public int calculaDiarias(String diaInicio, String diaFim) {
 		//separando dia/mês/ano
 		String dInicio[] = diaInicio.split("/");
 		String dFim[] = diaFim.split("/");
@@ -62,99 +57,25 @@ public class Periodo {
 		int mesF = Integer.parseInt(dFim[1]);
 		int anoF = Integer.parseInt(dFim[2]);
 		
-		//determinando meses
-		switch (mesI) {
-		case 1:
-			inicio.set(Calendar.MONTH, Calendar.JANUARY);
-			break;
-		case 2:
-			inicio.set(Calendar.MONTH, Calendar.FEBRUARY);
-			break;
-		case 3:
-			inicio.set(Calendar.MONTH, Calendar.MARCH);
-			break;
-		case 4:
-			inicio.set(Calendar.MONTH, Calendar.APRIL);
-			break;
-		case 5:
-			inicio.set(Calendar.MONTH, Calendar.MAY);
-			break;
-		case 6:
-			inicio.set(Calendar.MONTH, Calendar.JUNE);
-			break;
-		case 7:
-			inicio.set(Calendar.MONTH, Calendar.JULY);
-			break;
-		case 8:
-			inicio.set(Calendar.MONTH, Calendar.AUGUST);
-			break;
-		case 9:
-			inicio.set(Calendar.MONTH, Calendar.SEPTEMBER);
-			break;
-		case 10:
-			inicio.set(Calendar.MONTH, Calendar.OCTOBER);
-			break;
-		case 11:
-			inicio.set(Calendar.MONTH, Calendar.NOVEMBER);
-			break;
-		case 12:
-			inicio.set(Calendar.MONTH, Calendar.DECEMBER);
-			break;
+		//calculando diárias
+		if (anoF > anoI) {
+			int meses = mesF + (12 - mesI);
+			diarias = (30 * meses) - ((30 - diaI) + (30 - diaF));
+		} else {
+			if (mesF > mesI) {
+				int meses = mesF - mesI;
+				diarias = (30 * meses) - ((30 - diaI) + (30 - diaF));
+			} else {
+				diarias = diaF - diaI;
+			}
 		}
 		
-		switch (mesF) {
-		case 1:
-			fim.set(Calendar.MONTH, Calendar.JANUARY);
-			break;
-		case 2:
-			fim.set(Calendar.MONTH, Calendar.FEBRUARY);
-			break;
-		case 3:
-			fim.set(Calendar.MONTH, Calendar.MARCH);
-			break;
-		case 4:
-			fim.set(Calendar.MONTH, Calendar.APRIL);
-			break;
-		case 5:
-			fim.set(Calendar.MONTH, Calendar.MAY);
-			break;
-		case 6:
-			fim.set(Calendar.MONTH, Calendar.JUNE);
-			break;
-		case 7:
-			fim.set(Calendar.MONTH, Calendar.JULY);
-			break;
-		case 8:
-			fim.set(Calendar.MONTH, Calendar.AUGUST);
-			break;
-		case 9:
-			fim.set(Calendar.MONTH, Calendar.SEPTEMBER);
-			break;
-		case 10:
-			fim.set(Calendar.MONTH, Calendar.OCTOBER);
-			break;
-		case 11:
-			fim.set(Calendar.MONTH, Calendar.NOVEMBER);
-			break;
-		case 12:
-			fim.set(Calendar.MONTH, Calendar.DECEMBER);
-			break;
-		}
-		
-		//criando datas
-		inicio.set(Calendar.DAY_OF_MONTH, diaI);
-		inicio.set(Calendar.YEAR, anoI);
-		
-		fim.set(Calendar.DAY_OF_MONTH, diaF);
-		fim.set(Calendar.YEAR, anoF);
-		
-		//calculando período
-		Period diarias = Period.between(inicio, fim);
-	}*/
+		return diarias;		
+	}
 	
 	public String formataData(String diaInicio, String diaFim, String horaInicio, String horaFim) {
 		data = ("\nIníco: " + diaInicio + " - " + horaInicio + "\nFim " + diaFim + " - " + horaFim
-				+ "\nDiárias calculadas: " /*+ diarias*/);
+				+ "\nDiárias calculadas: " + diarias);
 		return data;
 	}
 
